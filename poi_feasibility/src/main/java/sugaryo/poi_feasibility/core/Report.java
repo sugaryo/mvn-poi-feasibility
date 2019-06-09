8bc8a35698c777a7e3df4ccbeec93058a818e08b
sugaryo <ryo.sugawara.1224@gmail.com>
2019-06-09 20:38:16 +0900

    テンプレートファイルを追加。

--- poi_feasibility/src/main/java/sugaryo/poi_feasibility/core/Report.java
+++ poi_feasibility/src/main/java/sugaryo/poi_feasibility/core/Report.java
@@ -1,5 +1,7 @@
 package sugaryo.poi_feasibility.core;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 import sugaryo.poi_feasibility.utility.ExcelWrapper;
 
 public class Report {
@@ -7,8 +9,9 @@ public class Report {
 	public void output() {
 		
 		// 取り敢えず新規にブックを作って保存する。
-		try ( var excel = new ExcelWrapper() ) { 
+		try ( var excel = new ExcelWrapper( new XSSFWorkbook( "resource/template.xlsx" ) ) ) {
 			excel.cell( 0, 0 ).value( "test" );
+			excel.cell( "DATA" ).value( "名前でセル参照" );
 			excel.save( "C:/test/poi/test.xlsx" );
 		}
 		// 検査例外は RuntimeException でラップしてスローする。
